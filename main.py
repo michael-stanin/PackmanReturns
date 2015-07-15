e79b4e8ac95a2cc46e546c2265044117a2bc3ee3
Michael Stanin <michael.stanin@gmail.com>
2015-07-16 00:23:35 +0300

    Initial commit for Packman Returns

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello World")
+import pygame
+from pygame.locals import *
+
+
+class Game:
+
+    def __init__(self):
+        self.status = True
+
+    def start(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((640, 480))
+
+        while self.status:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
+
+game = Game()
+game.start()
