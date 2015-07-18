7e0ecfb90d32c6e33abb0fad5a6ea2e150f799e9
Michael Stanin <michael.stanin@gmail.com>
2015-07-18 15:12:20 +0300

    Adding packman to the stage.

--- src/main.py
+++ src/main.py
@@ -1,22 +1,34 @@
 import pygame
-from pygame.locals import *
+
+from packman import *
 
 
-WIDTH   = 700
-HEIGHT  = 500
-CAPTION = "Packman Returns"
+CAPTION            = "Packman Returns"
+BACKGROUND_IMAGE   = "../resources/backgrounds/2.jpg"
+PACKMAN_IMAGE_FILE = "../resources/sprites/24x24packman.png"
 
 class Game:
 
     def __init__(self):
-        self.status   = True
-        self.size     = (WIDTH, HEIGHT)
-        self.caption  = CAPTION
+        self.status      = True
+        self.size        = (WIDTH, HEIGHT)
+        self.caption     = CAPTION
+        self.dirty_rects = []
 
     def start(self):
-        self._init_pygame()      
+        self._init_pygame()
+
+        self.background = pygame.image.load(BACKGROUND_IMAGE)
+        self.dirty_rects.append(self.background.get_rect())
+
+        self._load_sprites()
+
+        clock = pygame.time.Clock()        
 
         while self.status:
+            # Limit frame speed to 60 FPS.
+            time_passed = clock.tick(60)
+
             for event in pygame.event.get():
                 self._on_event(event)
 
@@ -28,21 +40,53 @@ class Game:
 
     def _init_pygame(self):
         pygame.init()
-        pygame.display.set_mode(self.size)
+        self.screen = pygame.display.set_mode(self.size)
         pygame.display.set_caption(self.caption)
 
     def _on_event(self, event):
-        if event.type == pygame.QUIT:
+        if (event.type == pygame.QUIT) or (event.type == pygame.KEYDOWN and
+            event.key == K_ESCAPE):
             self.status = False
+        
 
     def _on_loop(self):
-        pass
+        self.screen.blit(self.background, ZERO_POINT)
+
+        self.packman_sprites.update()
+        self.packman_sprites.draw(self.screen)
+
+        pygame.display.flip()
+        pygame.display.update(self.dirty_rects)
+
 
     def _on_render(self):
         pass
 
     def _on_cleanup(self):
         pygame.quit()
+
+    def _load_sprites(self):
+        self._load_packman()
+
+        self._load_ghosts()
+
+        self._load_walls()
+
+        self._load_pellets()
+
+    def _load_packman(self):
+        self.packman = Packman(PACKMAN_IMAGE_FILE, pygame.sprite.Group())
+        self.packman_sprites = pygame.sprite.RenderPlain(self.packman)
+        self.dirty_rects.append(self.packman.image.get_rect())
+        
+    def _load_ghosts(self):
+        pass
+
+    def _load_walls(self):
+        pass
+
+    def _load_pellets(self):
+        pass
 
 game = Game()
 game.start()
--- src/packman.py
+++ src/packman.py
@@ -1,42 +1,44 @@
-from pygame.locals import sprite.Sprite as MichaelSprite
+import pygame
+from pygame.locals import *
 
 
 RIGHT_ANGLE_DEGREES = 90
 ZERO_POINT          = (0, 0)
 ZERO                = 0
-WIDTH               = 700
-HEIGHT              = 500
+WIDTH               = 960
+HEIGHT              = 640
 PACKMAN_STEP        = 8
 
-class Packman(MichaelSprite):
+class Packman(pygame.sprite.Sprite):
 
-    def __init__(self, image=None, *args, **kwargs):
+    def __init__(self, image=None, *groups):
+        super(Packman, self).__init__(*groups)
         self.pellets      = ZERO
-        self.image        = image
+        self.image        = pygame.image.load(image)
         self.image_right  = self.image
         self.image_left   = pygame.transform.flip(self.image, True, False)
-        self.image_up     = pygame.transform.rotate(self.image, RIGHT_ANGLE_DEGREES
+        self.image_up     = pygame.transform.rotate(self.image, RIGHT_ANGLE_DEGREES)
         self.image_down   = pygame.transform.rotate(self.image, -RIGHT_ANGLE_DEGREES)
         self.rect         = pygame.rect.Rect(ZERO_POINT, self.image.get_size())
 
     def update(self):
-        # Restrictions.
-        self._restrictions()
-        
         key = pygame.key.get_pressed()
         # Actual movement.
         self._movement(key)
+
+        # Restrictions
+        self._restrictions()
 
     def _restrictions(self):
         self._window_restrictions()
 
     def _window_restrictions(self):
-        if self.rect.y >= HEIGHT:
-            self.rect.y = HEIGHT
+        if self.rect.y + PACKMAN_STEP >= HEIGHT:
+            self.rect.y = HEIGHT - PACKMAN_STEP - self.image.get_size()[1]
         elif self.rect.y <= ZERO:
             self.rect.y = ZERO
-        if self.rect.x >= WIDTH:
-            self.rect.x = WIDTH
+        if self.rect.x + PACKMAN_STEP >= WIDTH:
+            self.rect.x = WIDTH - PACKMAN_STEP - self.image.get_size()[0] 
         if self.rect.x <= ZERO:
             self.rect.x = ZERO
 
