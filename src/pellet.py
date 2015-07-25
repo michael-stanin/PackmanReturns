773abad0c75ab6fe8834485bc0c676f0c2ca348f
Michael Stanin <michael.stanin@gmail.com>
2015-07-25 14:51:07 +0300

    Adding some more pellets.

--- src/pellet.py
+++ src/pellet.py
@@ -1,39 +1,17 @@
-#from block import *
-import pygame
-from pygame import gfxdraw
+import gameobj
 
-GOLD       = (255,215,0)
-GOLDEN_ROD = (218,165,32)
+
 RADIUS     = 8
 
 
 PELLET_IMAGE_FILE        = "../resources/sprites/pellet.png"
 
 
-class Pellet(pygame.sprite.Sprite):
+class Pellet(gameobj.GameObj):
 
-	def __init__(self, x, y, energized, surface):
+	def __init__(self, x, y, energized):
 		#super(Pellet, self).__init__(x, y, width, height)
 		# Call the parent's constructor
-		super().__init__()
-		if energized:
-			color = GOLD
-		else:
-			color = GOLDEN_ROD
-
-		self.image             = pygame.image.load(PELLET_IMAGE_FILE)
-		self.rect              = self.image.get_rect()
-		self.rect.x            = x
-		self.rect.y            = y
-
-		"""
-		pygame.gfxdraw.aacircle(surface, x, y, 3, color)
-		pygame.gfxdraw.filled_circle(surface, x, y, 3, color)
-		self.rect.x = x
-		self.rect.y = y
-		"""
-
-		#self.image.fill(color)
-		#self.rect = pygame.draw.circle(self.image, color, (x, y), RADIUS, 8)
+		super().__init__(x, y, PELLET_IMAGE_FILE)
 		self.energized = energized
 
